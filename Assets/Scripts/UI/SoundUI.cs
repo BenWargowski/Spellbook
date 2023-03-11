edb91cc24321cff113680fd717b54f468b698827
dlh123123 <davidlh1@uci.edu>
2023-03-11 14:35:44 -0800

    Updated sound system and misc changes
    Sound settings use player prefs and now includes toggle feature.

--- Assets/Scripts/UI/SoundUI.cs
+++ Assets/Scripts/UI/SoundUI.cs
@@ -11,42 +11,78 @@ public class SoundUI : MonoBehaviour
 
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
-
-    [SerializeField] private AudioSource sfxSource;
+    [SerializeField] private Toggle musicToggle;
+    [SerializeField] private Toggle sfxToggle;
 
     public const string mixerMusic = "MusicVolume";
     public const string mixerSFX = "SFXVolume";
+    public const string mixerMusicToggled = "MusicToggled";
+    public const string mixerSFXToggled = "SFXToggle";
 
     void Awake()
     {
         musicSlider.onValueChanged.AddListener(SetMusicVolume);
         sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+        musicToggle.onValueChanged.AddListener(SetMusicToggle);
+        sfxToggle.onValueChanged.AddListener(SetSFXToggle);
     }
 
     void Start()
     {
-        float musicValue = 0f;
-        float sfxValue = 0f;
+        float musicVolume = PlayerPrefs.GetFloat(mixerMusic);
+        float sfxVolume = PlayerPrefs.GetFloat(mixerSFX);
 
-        mixer.GetFloat(mixerMusic, out musicValue);
-        mixer.GetFloat(mixerSFX, out sfxValue);
+        bool musicIsToggled = PlayerPrefs.GetInt(mixerMusicToggled, 1) == 1 ? true : false;
+        bool sfxIsToggled = PlayerPrefs.GetInt(mixerSFXToggled, 1) == 1 ? true : false;
 
-        musicSlider.value = musicValue;
-        sfxSlider.value = sfxValue;
-    }
+        SetMusicVolume(musicVolume);
+        musicSlider.value = musicVolume;
 
-    public void PlaySound(AudioClip clip)
-    {
-        sfxSource.PlayOneShot(clip);
+        SetSFXVolume(sfxVolume);
+        sfxSlider.value = sfxVolume;
+
+        SetMusicToggle(musicIsToggled);
+        musicToggle.isOn = musicIsToggled;
+
+        SetSFXToggle(sfxIsToggled);
+        sfxToggle.isOn = sfxIsToggled;
     }
 
     private void SetMusicVolume(float value)
     {
         mixer.SetFloat(mixerMusic, value);
+        PlayerPrefs.SetFloat(mixerMusic, value);
+
+        SetMusicToggle(true);
+        musicToggle.isOn = true;
     }
 
     private void SetSFXVolume(float value)
     {
         mixer.SetFloat(mixerSFX, value);
+        PlayerPrefs.SetFloat(mixerSFX, value);
+
+        SetSFXToggle(true);
+        sfxToggle.isOn = true;
+    }
+
+    private void SetMusicToggle(bool isToggled)
+    {
+        if (isToggled)
+            mixer.SetFloat(mixerMusic, musicSlider.value);
+        else
+            mixer.SetFloat(mixerMusic, -80f);
+
+        PlayerPrefs.SetInt(mixerMusicToggled, isToggled ? 1 : 0);
+    }
+
+    private void SetSFXToggle(bool isToggled)
+    {
+        if (isToggled)
+            mixer.SetFloat(mixerSFX, sfxSlider.value);
+        else
+            mixer.SetFloat(mixerSFX, -80f);
+
+        PlayerPrefs.SetInt(mixerSFXToggled, isToggled ? 1 : 0);
     }
 }
